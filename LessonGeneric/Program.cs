113234367f3642a149c750c75e694a04e96ee0ef
naz9559 <naz9559@gmail.com>
2021-07-15 13:24:22 +0300

    Add Apple and Banana

--- LessonGeneric/Program.cs
+++ LessonGeneric/Program.cs
@@ -10,11 +10,15 @@ namespace LessonGeneric
             var product = new Product("Apple",400,100,5);
             var product2 = new Product("Banana",300,200,20);
             Bag<Product> bag = new Bag<Product>();
+            Apple apple = new Apple("Golden",100,70,5);
+            Banana banana = new Banana("Cavendish",100,90,10);
             bag.Calculate(product);
             bag.Calculate(product2);
+            bag.Calculate(apple);
+            bag.Calculate(banana);
+
             Console.WriteLine($"Price: {bag.Price} grn");
-            //var list = new List<int>();
-            //var dict = new Dictionary<int, string>();
+           
         }
     }
 }
